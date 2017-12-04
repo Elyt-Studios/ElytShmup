9fab7fefb5218defe52ff208b064c69e99f44fda
plan122 <paniruru2@gmail.com>
2017-12-04 22:24:01 +1300

    !UPDATE
    1...I made a small test arena type thingy to play around with lazers etc
    
    2...Updated Bullet Prefab
    
    3...NEW SCRIPT!!! EnemyAI the script tracks the player and move towards it I still have to convert it to work with 2D as it is for a 3D spae but it is still in debug mode so you can play around with it.

--- 1/scence/Assets/Scripts/SetTiles.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(MeshRenderer))] public class SetTiles : MonoBehaviour { public Vector2 tiling;
-	void Start () {
-		GetComponent<MeshRenderer>().material.mainTextureScale = tiling;
-	}    
-}
